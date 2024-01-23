b81ebb87c81f83b0fe3e1fa725be3e94b6b11497
hyooo11 <tlsgywls1107@naver.com>
2024-01-23 19:11:59 +0900

    [ADD] productDetail

--- src/component/product/ProductDetail.js
+++ src/component/product/ProductDetail.js
@@ -1,20 +1,51 @@
+"use client";
+import { useEffect, useState } from "react";
 import style from "./productDetail.module.css";
 
 const ProductDetail = ({ productDetail }) => {
   console.log(productDetail);
+  const [snackList, setSnackList] = useState([]);
+
+  useEffect(() => {
+    if (productDetail !== undefined) {
+      setSnackList(productDetail.recommendedSnack.split(" / "));
+    }
+  }, [productDetail]);
+
+  console.log(snackList);
+
   return (
     <div className="maxframe sub_p_wrap">
-      <div>
-        <div className={style.thumnail}>
-          <figure>
-            <img src={productDetail.imageUrl} />
-          </figure>
-        </div>
-        <div className={style.main_txt_box}>
-          <h3>{productDetail.korName}</h3>
-          <span>{productDetail.engName}</span>
-        </div>
-      </div>
+      <>
+        {productDetail && (
+          <section className={style.sec_1}>
+            <div className={style.thumnail}>
+              <figure>
+                <img src={productDetail.imageUrl} />
+              </figure>
+            </div>
+            <div className={style.main_txt_box}>
+              <h3>{productDetail.korName}</h3>
+              <span>{productDetail.engName}</span>
+              <div className={style.snack_icon_wrap}>
+                {snackList.map((data, index) => {
+                  return (
+                    <>
+                      {data !== "null" ? (
+                        <span>
+                          <img src={`/media/icon/${data}.PNG`} />
+                        </span>
+                      ) : (
+                        <></>
+                      )}
+                    </>
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+        )}
+      </>
     </div>
   );
 };
